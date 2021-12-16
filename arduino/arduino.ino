95dc62779510790d0953c7d3b27dc513eabc70c6
rizal-anggoro <rizalanggoro78@gmail.com>
2021-12-16 22:35:07 +0700

    Update arduino.ino

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,6 +1,9 @@
 #include "DeskPixel.h"
 #include "MatrixPixel.h"
 #include "Segment.h"
+#include "Communication.h"
+
+#include <Wire.h>
 
 DeskPixel deskPixel;
 MatrixPixel matrixPixel;
@@ -8,14 +11,46 @@ Segment segment;
 void setup() {
   Serial.begin(115200);
 
+  Wire.begin(1);
+  Wire.onReceive(receiveEvent);
+  
   deskPixel.begin();
   matrixPixel.begin();
 
-  String data = "0164";
-  byte bitmap[11];
-  segment.getBitmapFromData(data, bitmap);
-  matrixPixel.displayBitmap(bitmap);
+  deskPixel.test(255, 0, 255);
+
+//  String data = "0164";
+//  byte bitmap[11];
+//  segment.getBitmapFromData(data, bitmap);
+//  matrixPixel.displayBitmap(bitmap);
 }
 
 void loop() {
+}
+
+void receiveEvent(int how_many) {
+  char buffer[how_many];
+  int index = 0;
+  while (Wire.available() > 0) {
+    char data = Wire.read();
+    buffer[index] = data;
+    index++;
+  }
+  String receive_data = String(buffer);
+  receive_data.trim();
+
+  parseSerial(receive_data);
+}
+
+void parseSerial(String data) {
+  int a = data.indexOf(";");
+  int b = data.lastIndexOf(";");
+  String c = data.substring(0, a);
+  String d = data.substring((a + 1), b);
+
+  if (c == "/clock_data") {
+    byte bitmap[11];
+    segment.getBitmapFromData(d, bitmap);
+    matrixPixel.displayBitmap(bitmap);
+  }
 }
