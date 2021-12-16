602277c6671896ce5a30f2e06a6618b224b4a365
rizal-anggoro <rizalanggoro78@gmail.com>
2021-12-16 21:42:31 +0700

    Update nodemcu.ino

--- nodemcu/nodemcu.ino
+++ nodemcu/nodemcu.ino
@@ -1,8 +1,17 @@
+#include "Wifi.h"
+#include "Ntp.h"
+
+Wifi wifi;
+Ntp ntp;
 void setup() {
   Serial.begin(115200);
+
+  wifi.open();
+  ntp.begin();
 }
 
 void loop() {
-  Serial.println("called");
+  String a = ntp.getHMClock();
+  Serial.println(a);
   delay(1000);
 }
