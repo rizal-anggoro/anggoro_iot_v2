7ce98770edb7d7b0ca9d77d72b42e87f8cb0e7d1
rizal-anggoro <rizalanggoro78@gmail.com>
2021-12-16 21:56:36 +0700

    Update nodemcu.ino

--- nodemcu/nodemcu.ino
+++ nodemcu/nodemcu.ino
@@ -1,17 +1,29 @@
 #include "Wifi.h"
 #include "Ntp.h"
+#include "Communication.h"
 
 Wifi wifi;
 Ntp ntp;
+Communication communication;
 void setup() {
   Serial.begin(115200);
 
+  communication.begin();
   wifi.open();
   ntp.begin();
 }
 
 void loop() {
-  String a = ntp.getHMClock();
-  Serial.println(a);
-  delay(1000);
+  streamClock();
+}
+
+unsigned long previous_millis = 0;
+void streamClock() {
+  unsigned long current_millis = millis();
+  if (current_millis - previous_millis >= 1000) {
+    previous_millis = current_millis;
+    String data = ntp.getHMClock();
+    String s_data = "/clock_data;" + data;
+    communication.sendString(s_data);
+  }
 }
