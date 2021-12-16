df5ef3cbb29b35ec693ff9d7a766c9389b27bdd0
rizal-anggoro <rizalanggoro78@gmail.com>
2021-12-16 08:18:11 +0700

    Membuat SegmentData
    SegmentData digunakan untuk menyimpan dan mengolah data matrix dalam bentuk byte yang kemudian akan ditampilkan ke neopixel.

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -9,8 +9,17 @@ void setup() {
   deskPixel.begin();
   matrixPixel.begin();
   
-  deskPixel.test(255, 0, 255);
-  matrixPixel.test(0, 255, 255);
+//  deskPixel.test(255, 0, 255);
+//  matrixPixel.test(0, 255, 255);
+
+  byte a = B00000000;
+  byte b = B00000111;
+  byte c = B00000101;
+  a = (b << 4) | (c);
+  for (int i = 7; i >= 0; i--) {
+    Serial.print(bitRead(a, i));
+  }
+  Serial.println();
 }
 
 void loop() {
