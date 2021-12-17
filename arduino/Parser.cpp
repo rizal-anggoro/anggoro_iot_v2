a4fc5ee9ff379aa909df4ca67332450dbee4f504
rizal-anggoro <rizalanggoro78@gmail.com>
2021-12-17 22:13:53 +0700

    Membuat kelas parser

---
+++ arduino/Parser.cpp
@@ -0,0 +1,16 @@
+#include "Parser.h"
+
+extern MatrixPixel matrixPixel;
+
+void Parser::let(String receive_data) {
+  Serial.println(receive_data);
+  
+  int a = receive_data.indexOf(";");
+  int b = receive_data.lastIndexOf(";");
+  String data_path = receive_data.substring(0, a);
+  String data = receive_data.substring((a + 1), b);
+
+  if (data_path.indexOf("/matrix/brightness") != -1) {
+    matrixPixel.setBrightness(data.toInt());
+  }
+}
---
+++ arduino/Parser.h
@@ -0,0 +1,15 @@
+#ifndef Parser_h
+#define Parser_h
+
+#include <Arduino.h>
+
+#include "MatrixPixel.h"
+
+class Parser {
+  private:
+  
+  public:
+  void let(String receive_data);
+};
+
+#endif
